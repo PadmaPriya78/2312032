e923655862957ed0bdbc2da614ae52cb14589d2d
PadmaPriya78 <2312032@nec.edu.in>
2026-07-02 12:01:36 +0530

    lgger 3

--- logging-middleware/logger.js
+++ logging-middleware/logger.js
@@ -1,20 +1,31 @@
 export async function Log(token, stack, level, packageName, message) {
-    const response = await fetch(
-        "http://4.224.186.213/evaluation-service/logs",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                stack,
-                level,
-                package: packageName,
-                message
-            })
-        }
-    );
+    try {
+        const response = await fetch(
+            "http://4.224.186.213/evaluation-service/logs",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    stack,
+                    level,
+                    package: packageName,
+                    message
+                })
+            }
+        );
 
-    const data = await response.json();
+        if (!response.ok) {
+            throw new Error("Failed to create log");
+        }
+
+        const data = await response.json();
+
+        return data;
+
+    } catch (error) {
+        throw error;
+    }
 }
